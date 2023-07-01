4a7a2be800fe981460fbb3bb2faee312a0005fcd
Max3281 <113448092+Max3281@users.noreply.github.com>
2023-07-01 14:37:09 +0300

    mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Jack:naYLGJxh3pr3vGm4@cluster0.7awa50v.mongodb.net/contactsList?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+
+// naYLGJxh3pr3vGm4
